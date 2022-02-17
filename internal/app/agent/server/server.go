f46e11edfd4a3f7aabf813f2b42512146b3081f5
dollarkillerx <adapawang@gmail.com>
2022-02-17 15:55:09 +0800

    zip

--- internal/app/agent/server/server.go
+++ internal/app/agent/server/server.go
@@ -1,11 +1,19 @@
 package server
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"path"
+	"path/filepath"
 	"time"
 
 	"github.com/dollarkillerx/fireworks/internal/conf"
+	"github.com/dollarkillerx/fireworks/internal/pkg/models"
+	"github.com/dollarkillerx/fireworks/internal/pkg/utils"
 	"github.com/dollarkillerx/urllib"
 )
 
@@ -37,6 +45,69 @@ func (a *AgentServer) Run() error {
 			continue
 		}
 
+		if code != 200 {
+			log.Println(string(resps))
+			continue
+		}
+
+		var agent models.Agent
+		err = json.Unmarshal(resps, &agent)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+	}
+
+	return nil
+}
+
+func (a *AgentServer) deploy(agent models.Agent) {
+	var err error
+	os.MkdirAll(agent.TaskName, 00766)
+	os.MkdirAll(path.Join(agent.TaskName, "files"), 00766)
+	os.MkdirAll(path.Join(agent.TaskName, "images"), 00766)
+
+	err = ioutil.WriteFile(path.Join(agent.TaskName, "docker-compose.yaml"), agent.DockerCompose, 00666)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = a.download(path.Join(agent.TaskName, "files"), agent.Files)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	err = a.download(path.Join(agent.TaskName, "images"), agent.DockerImages)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+}
+
+func (a *AgentServer) download(path string, fp string) error {
+	if fp == "" {
+		return nil
+	}
+	code, resp, err := urllib.Get(fmt.Sprintf("%s/download/%s", a.conf.BackendAddr, fp)).SetHeader("token", a.conf.Token).Byte()
+	if err != nil {
+		return err
+	}
+
+	if code != 200 {
+		return errors.New(string(resp))
+	}
+
+	fileName := filepath.Join(path, "data.zip")
+	err = ioutil.WriteFile(fileName, resp, 00766)
+	if err != nil {
+		return err
+	}
+
+	err = utils.DeCompress(fileName, path)
+	if err != nil {
+		return err
 	}
 
 	return nil
--- test/dir_test.go
+++ test/dir_test.go
@@ -7,6 +7,8 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
+
+	"github.com/dollarkillerx/fireworks/internal/pkg/utils"
 )
 
 func TestDir(t *testing.T) {
@@ -23,4 +25,22 @@ func TestDir(t *testing.T) {
 	}
 
 	fmt.Println(string(out))
+}
+
+func TestZip(t *testing.T) {
+	open, err := os.Open("../internal")
+	if err != nil {
+		panic(err)
+	}
+
+	err = utils.Compress([]*os.File{open}, "data.zip")
+	if err != nil {
+		panic(err)
+	}
+
+	os.MkdirAll("data", 00755)
+	err = utils.DeCompress("data.zip", "data")
+	if err != nil {
+		panic(err)
+	}
 }
